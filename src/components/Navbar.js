ed67fbc937e78f44a70ffa2cfebdfe7db78385a7
Joemn <Aruljoe37@gmail.com>
2021-01-13 21:36:55 +0530

    nav2 added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,17 +32,33 @@ function Navbar() {
         <div className="nav__level1__items__Logo">
           <Image className="logo" fluid={data.file.childImageSharp.fluid} />
         </div>
-        <div onClick={triggerHandle} className="nav__level1__items__Trigger">
+        <div
+          onClick={triggerHandle}
+          className={`nav__level1__items__Trigger ${navOpen && "navOpen"}`}
+        >
           <span></span>
           <span></span>
           <span></span>
         </div>
+      </div>
+      <div className="bigscrennNav">
+        <Link to="/">Home</Link>
+        <Link to="/">About</Link>
+        <Link to="/cakes">Cakes</Link>
+        <Link to="/surpricePacks">SurpricePacks</Link>
+        <Link to="/">
+          <span className="cartICon">
+            <FiShoppingCart />
+          </span>
+        </Link>
+        <Link to="/">Contact Us</Link>
       </div>
       <div
         className={`nav__level1__items__Home ${
           navOpen ? "navisOpen" : "navisClosed"
         } `}
       >
+        <Image className="sidelogo" fluid={data.file.childImageSharp.fluid} />
         <Link to="/">Home</Link>
         <Link to="/">About</Link>
         <Link to="/cakes">Cakes</Link>
