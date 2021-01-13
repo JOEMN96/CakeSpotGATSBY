c9a17c84ac35c4fa42de8c2ecca5a272d10d6968
Joemn <Aruljoe37@gmail.com>
2021-01-13 06:34:28 +0530

    nav v1 added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import { graphql, useStaticQuery } from "gatsby";
 import Image from "gatsby-image";
+import { FiShoppingCart } from "react-icons/fi";
 
-function Navbar() {
-  const data = useStaticQuery(graphql`
-    query logo {
-      file(name: { eq: "logo" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid
-          }
+const query = graphql`
+  query logo {
+    file(name: { eq: "logo" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
         }
       }
     }
-  `);
+  }
+`;
 
-  console.log(data);
+function Navbar() {
+  const data = useStaticQuery(query);
+
+  const [navOpen, setnavOpen] = useState(false);
+
+  const triggerHandle = () => {
+    setnavOpen(!navOpen);
+    console.log(navOpen);
+  };
 
   return (
     <nav>
@@ -24,16 +32,27 @@ function Navbar() {
         <div className="nav__level1__items__Logo">
           <Image className="logo" fluid={data.file.childImageSharp.fluid} />
         </div>
-        <div className="nav__level1__items__Trigger">
+        <div onClick={triggerHandle} className="nav__level1__items__Trigger">
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-      <div className="nav__level1__items__Home">
+      <div
+        className={`nav__level1__items__Home ${
+          navOpen ? "navisOpen" : "navisClosed"
+        } `}
+      >
         <Link to="/">Home</Link>
+        <Link to="/">About</Link>
         <Link to="/cakes">Cakes</Link>
         <Link to="/surpricePacks">SurpricePacks</Link>
+        <Link to="/">
+          <span className="cartICon">
+            <FiShoppingCart />
+          </span>
+        </Link>
+        <Link to="/">Contact Us</Link>
       </div>
     </nav>
   );
